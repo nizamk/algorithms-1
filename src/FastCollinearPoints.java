8473d114787d1304044320a2bbd213ba7f16f5b6
nizamuddin.ariffin <nizam.ariffin@gmail.com>
2016-12-06 10:48:21 +0800

    added basic LineSegment, BruteCollinear, and FastCollinear classes

---
+++ src/FastCollinearPoints.java
@@ -0,0 +1,29 @@
+public class FastCollinearPoints {
+
+    /**
+     * finds all line segments containing 4 points or more points
+     *
+     * @param points
+     */
+    public FastCollinearPoints(Point[] points) {
+
+    }
+
+    /**
+     * The number of line segments
+     *
+     * @return
+     */
+    public int numberOfSegments() {
+        return 0;
+    }
+
+    /**
+     * the line segments
+     *
+     * @return
+     */
+    public LineSegment[] segments() {
+        return new LineSegment[0];
+    }
+}
